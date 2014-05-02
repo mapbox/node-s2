1a37958218bf74b44c0d3c833ec94bea791bf525
Dane Springmeyer <dane@mapbox.com>
2014-05-02 13:29:12 -0700

    fix #1

--- geometry/base/logging.h
+++ geometry/base/logging.h
@@ -35,7 +35,7 @@ using std::endl;
 #ifndef NDEBUG
 // Debug-only checking.
 #define DCHECK(condition) CHECK(condition)
-#define DCHECK_EQ(val1, val2) CHECK(val1, val2)
+#define DCHECK_EQ(val1, val2) CHECK_EQ(val1, val2)
 #define DCHECK_NE(val1, val2) CHECK_NE(val1, val2)
 #define DCHECK_LE(val1, val2) CHECK_LE(val1, val2)
 #define DCHECK_LT(val1, val2) CHECK_LT(val1, val2)
