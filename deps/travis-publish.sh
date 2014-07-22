9c56d8558afcc53f21d192d4d91529fc67f3dc68
Mick Thompson <dthompson@gmail.com>
2014-07-22 16:06:49 -0700

    only publish when told to. [publish binary]

--- deps/travis-publish.sh
+++ deps/travis-publish.sh
@@ -3,16 +3,16 @@
 set -e
 
 # Inspect binary.
-# if [ $platform == "linux" ]; then
-#     ldd ./lib/s2.node
-# else
-#     otool -L ./lib/s2.node
-# fi
+if [ $platform == "linux" ]; then
+    ldd ./build/Release/_s2.node
+else
+    otool -L ./build/Release/_s2.node
+fi
 
-# COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
-#
-# if test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"
-#     then
+COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+
+if test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"
+    then
 
     npm install aws-sdk
     ./node_modules/.bin/node-pre-gyp package testpackage
@@ -24,4 +24,4 @@ set -e
 
     npm install aws-sdk
     node-pre-gyp info
-# fi
+fi
