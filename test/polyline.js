3ff4e5cdbf7575779747218db4a2468741ebd9e5
Dennis Luxen <info@project-osrm.org>
2014-08-07 13:53:02 +0200

    add failing test

--- test/polyline.js
+++ test/polyline.js
@@ -2,9 +2,8 @@ var test = require('tap').test,
     s2 = require('../');
 
 test('S2Polyline', function(t) {
-
-    // var polyline = new s2.S2Polyline();
-    // t.ok(polyline, 'generates cell object');
+    var polyline = new s2.S2Polyline();
+    t.ok(polyline, 'generates cell object');
 
     t.end();
 });
