479c18439866014cd901976fae53dbea0eb430a1
Dennis Luxen <info@project-osrm.org>
2014-08-07 17:29:15 +0200

    add a couple of basic tests to Polyline class

--- test/polyline.js
+++ test/polyline.js
@@ -2,8 +2,27 @@ var test = require('tap').test,
     s2 = require('../');
 
 test('S2Polyline', function(t) {
-    var polyline = new s2.S2Polyline();
-    t.ok(polyline, 'generates cell object');
 
+    // throws on empty c'tor
+    t.throws(function() {
+        var polyline = new s2.S2Polyline();
+    }, 'throws on empty input');
+
+    // throws if parameter to c'tor isn't an array
+    t.throws(function() {
+        var latLng = new s2.S2LatLng([10, 20]);
+        var polyline = new s2.S2Polyline(latLng);
+    }, 'throws on non-array input');
+
+    // generates 10 distinct latlngs and passes them to c'tor
+    var coordinateArray = [];
+    var numberofCoordinates = 10;
+    for (var i = 0; i < 10; i++) {
+        var ll = new s2.S2LatLng(10*i+1, 5*i+1);
+        coordinateArray.push(ll);
+    };
+    var polyline = new s2.S2Polyline(coordinateArray);
+
+    t.ok(polyline.size() == 10, "cardinality of polyline");
     t.end();
 });
