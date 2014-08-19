08ca4f6ba3dcb20fc6d2a8994448891d17b51b6f
Dennis Luxen <info@project-osrm.org>
2014-08-19 11:55:21 +0200

    use proper C++11 includes

--- geometry/base/casts.h
+++ geometry/base/casts.h
@@ -10,9 +10,9 @@
 #ifndef BASE_CASTS_H_
 #define BASE_CASTS_H_
 
-#include <assert.h>         // for use with down_cast<>
-#include <string.h>         // for memcpy
-#include <limits.h>         // for enumeration casts and tests
+#include <cassert>         // for use with down_cast<>
+#include <cstring>         // for memcpy
+#include <climits>         // for enumeration casts and tests
 #include <typeinfo>         // for enumeration casts and tests
 #include <type_traits>
 
